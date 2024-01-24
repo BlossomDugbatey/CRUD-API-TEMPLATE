f99e3a3a034ae943969d0aaf9e76b7c543855b4c
Blossom Dugbatey <41967164+BlossomDugbatey@users.noreply.github.com>
2024-01-24 20:18:09 +0000

    setup

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import bodyParser from 'body-parser'; /**allows us take in incoming post request */
+// import bodyParser from 'body-parser'; /**allows us take in incoming post request */
 
 const app = express();
 
 const PORT = 5001;
 
-app.use(bodyParser.json()); /** meaning we'll be using json data in our whole application */
+app.use('/', (req, res)=> res.json('Hello World!') )
+
+// app.use(bodyParser.json()); /** meaning we'll be using json data in our whole application */
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
