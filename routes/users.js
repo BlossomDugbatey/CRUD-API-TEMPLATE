2aa53dc6e36f2afe454ddad9990f5f26bcc4774d
Blossom Dugbatey <41967164+BlossomDugbatey@users.noreply.github.com>
2024-01-26 00:28:10 +0000

    update user

--- routes/users.js
+++ routes/users.js
@@ -37,4 +37,17 @@ router.delete('/:id', (req, res)=> {
 
     res.send(`user with the is ${id} has been deleted`);
 })
+
+//update user 
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age} = req.body;
+
+    const user = users.find((user) => user.id === id);
+    if(firstName) user.firstName = firstName;
+    if(lastName) user.lastName = lastName;
+    if(age) user.age = age;
+
+    res.send(`user with the is ${id} has been updated`)
+})
 export default router;
